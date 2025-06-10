ac4a9c6f8e6299f25974a282c9be3ad59f2bff72
wilsongachungi <gachungiwilson91@gmail.com>
2025-06-10 21:16:25 +0300

    setting the home page

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import {AiOutlineHome, AiOutlineShopping, AiOutlineLogin, AiOutlineUserAdd, AiOutlineShoppingCart} from 'react-icons/ai'
 import {FaHeart} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
@@ -6,8 +6,30 @@ import { useNavigate } from 'react-router-dom'
 import './Navigation.css'
 
 const Navigation = () => {
+      const [dropdownOpen,setDropdownOpen]  = useState(false)
+      const [showSidebar, setShowSidebar] = useState(false)
+
+      const toggleDropdown = () => {
+        setShowSidebar(!showSidebar)
+      }
+
+      const toggleSidebar = () => {
+        setDropdownOpen(!dropdownOpen)
+      }
+
+      const closeSideBar = () => {
+        setShowSidebar(false)
+      }
     return (
-        <div>Navigation</div>
+        <div style={{zIndex: 999}} className={`${showSidebar ? "hidden" : 'flex'} xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-black w-[4%] hover:w-[15%] h-[100vh] fixed`}
+        id='navigation-container'>
+            <div className="flex flex-col justify-center space-y-4">
+                <Link to='/' className="flex items-center transition-transform transform hover:translate-x-2">
+                <AiOutlineHome  className="mr-2 mt-[3rem]" size={26}/>
+                <span className='hidden nav-item-name mt-[3rem]'>HOME</span> {""}
+                </Link>
+            </div>
+        </div>
     )
 }
 
