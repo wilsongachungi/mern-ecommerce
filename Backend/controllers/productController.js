e127fa03ea97ecee60a7db33468af8fe1739fd36
wilsongachungi <gachungiwilson91@gmail.com>
2025-06-28 13:11:40 +0300

    fetching all products with limit

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -105,6 +105,16 @@ const fetchProductById = asyncHandler (async (req, res) => {
         res.status(404).json({error: "Product not found"})
         
     }
+});
+
+const fetchAllProducts = asyncHandler(async (req, res) => {
+    try {
+        const products = await Product.find({}).populate('category').limit(12).sort ({createAt: -1})
+        res.json(products);
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: "Server Error"})
+    }
 })
 
-export { addProduct, updateProductDetails, removeProduct, fetchProducts, fetchProductById };
+export { addProduct, updateProductDetails, removeProduct, fetchProducts, fetchProductById, fetchAllProducts };
--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -3,11 +3,12 @@ import formidable from "express-formidable";
 const router = express.Router()
 
 //cotrollers
-import {addProduct, updateProductDetails, removeProduct, fetchProducts, fetchProductById} from '../controllers/productController.js'
+import {addProduct, updateProductDetails, removeProduct, fetchProducts, fetchProductById, fetchAllProducts} from '../controllers/productController.js'
 import {authenticate, authorizedAdmin} from '../middleware/authMiddleware.js'
 import checkId from '../middleware/checkId.js'
 
 router.route('/').get(fetchProducts).post(authenticate, authorizedAdmin, formidable(), addProduct);
+router.route('/allproducts').get(fetchAllProducts)
 router.route('/:id').get(fetchProductById).put(authenticate, authorizedAdmin, formidable(), updateProductDetails)
 .delete(authenticate, authorizedAdmin, removeProduct)
 
