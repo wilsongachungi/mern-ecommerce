e633442490553f33320a198624003e667f57a570
wilsongachungi <gachungiwilson91@gmail.com>
2025-06-28 15:55:36 +0300

    rating and commenting the product

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -3,12 +3,13 @@ import formidable from "express-formidable";
 const router = express.Router()
 
 //cotrollers
-import {addProduct, updateProductDetails, removeProduct, fetchProducts, fetchProductById, fetchAllProducts} from '../controllers/productController.js'
+import {addProduct, updateProductDetails, removeProduct, fetchProducts, fetchProductById, fetchAllProducts, addProductReview} from '../controllers/productController.js'
 import {authenticate, authorizedAdmin} from '../middleware/authMiddleware.js'
 import checkId from '../middleware/checkId.js'
 
 router.route('/').get(fetchProducts).post(authenticate, authorizedAdmin, formidable(), addProduct);
 router.route('/allproducts').get(fetchAllProducts)
+router.route('/:id/reviews').post(authenticate, authorizedAdmin, addProductReview)
 router.route('/:id').get(fetchProductById).put(authenticate, authorizedAdmin, formidable(), updateProductDetails)
 .delete(authenticate, authorizedAdmin, removeProduct)
 
