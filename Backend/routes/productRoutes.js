8a063a95450c20006d5fd74e567bf437b078bb50
wilsongachungi <gachungiwilson91@gmail.com>
2025-06-28 12:56:23 +0300

    fetching products using the id

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -3,12 +3,12 @@ import formidable from "express-formidable";
 const router = express.Router()
 
 //cotrollers
-import {addProduct, updateProductDetails, removeProduct, fetchProducts} from '../controllers/productController.js'
+import {addProduct, updateProductDetails, removeProduct, fetchProducts, fetchProductById} from '../controllers/productController.js'
 import {authenticate, authorizedAdmin} from '../middleware/authMiddleware.js'
 import checkId from '../middleware/checkId.js'
 
 router.route('/').get(fetchProducts).post(authenticate, authorizedAdmin, formidable(), addProduct);
-router.route('/:id').put(authenticate, authorizedAdmin, formidable(), updateProductDetails)
+router.route('/:id').get(fetchProductById).put(authenticate, authorizedAdmin, formidable(), updateProductDetails)
 .delete(authenticate, authorizedAdmin, removeProduct)
 
 export default router;
